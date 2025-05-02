dac7fa8eb9eb6484a20ffdcc43c085cd09e394a9
Juan <jiffavaron@gmail.com>
2025-05-02 18:36:47 -0300

    Todos los structs agregados

--- backend/domain/gym_domain.go
+++ backend/domain/gym_domain.go
@@ -6,4 +6,23 @@ type usuario struct {
 	PasswordHash string `json:"password"`
 	Nombre       string `json:"nombre"`
 	Admin        bool   `json:"admin"`
+	Foto         string `json:"foto"`
+}
+
+type actividad struct {
+	ID          int    `json:"id"`
+	Nombre      string `json:"nombre"`
+	Descripcion string `json:"descripcion"`
+	Dia         string `json:"dia"`
+	Horario     string `json:"horario"`
+	Cupos       int    `json:"cupos"`
+	Categoria   string `json:"categoria"`
+	Instructor  string `json:"instructor"`
+	Foto        string `json:"foto"`
+}
+
+type inscripcion struct {
+	IDUsuario        int    `json:"id_usuario"`
+	IDActividad      int    `json:"id_actividad"`
+	FechaInscripcion string `json:"fecha_inscripcion"`
 }
