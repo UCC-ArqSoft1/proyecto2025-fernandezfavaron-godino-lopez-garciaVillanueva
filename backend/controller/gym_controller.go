4f36c81dea2ae8e501093ba32abbf3f3c8734def
Juan <jiffavaron@gmail.com>
2025-05-02 17:54:24 -0300

    Partes del backend agregadas

---
+++ backend/controller/gym_controller.go
@@ -0,0 +1,19 @@
+package controller
+
+/*
+import (
+	"strconv"
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+func GetHotel(context *gin.Context) {
+	HotelIdString := context.Param("id")
+	HotelId, err := strconv.Atoi(HotelIdString)
+	if err != nil {
+		context.String(http.StatusBadRequest, "ID invalido")
+		return
+	}
+	context.JSON(http.StatusOK, )
+}
+*/
--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,14 @@
 package main
+
+/*
+import (
+	"Proyecto/controller"
+	//"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.New()
+	router.GET("/hotels/:id", controller.GetHotel)
+	router.Run()
+}*/
