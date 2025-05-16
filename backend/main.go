45a976d74375e2e9978e9140e4a889bf4d596ec0
Juan <jiffavaron@gmail.com>
2025-05-16 15:22:19 -0300

    Movi migrado a database.go

--- backend/main.go
+++ backend/main.go
@@ -3,15 +3,12 @@ package main
 import (
 	"Proyecto/app"
 	"Proyecto/database"
-	"Proyecto/domain"
 )
 
 func main() {
 	database.ConectarBD()
 
 	// Migrar las tablas
-	db := database.GetDB()
-	db.AutoMigrate(&domain.Usuario{}, &domain.Actividad{}, &domain.Inscripcion{})
 	// App
 	app.StartApp()
 
