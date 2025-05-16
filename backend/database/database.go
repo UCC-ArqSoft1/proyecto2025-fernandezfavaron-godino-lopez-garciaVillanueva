a6dee62d46881d6544d975bdc6f9ad1908d28f7e
Juan <jiffavaron@gmail.com>
2025-05-16 15:18:25 -0300

    Tip

--- backend/database/database.go
+++ backend/database/database.go
@@ -1,7 +1,9 @@
 package database
 
 import (
+	"Proyecto/domain"
 	"fmt"
+	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -10,7 +12,18 @@ import (
 var DB *gorm.DB
 
 func ConectarBD() {
-	dsn := "franco:miclave123@tcp(127.0.0.1:3306)/club_actividades?charset=utf8mb4&parseTime=True&loc=Local"
+
+	/* Capaz tenemos que usar sqlite para implementar esto mas facilmente.
+	Esto lee la variable de entorno de DB_DSN para que podamos conectar cada uno a su base de datos en test. Y no usar sqlite
+	Linux
+	export DB_DSN="usuario:miclave123@tcp(127.0.0.1:3306)/basedatosnombre?charset=utf8mb4&parseTime=True&loc=Local"
+	Windows Powershell
+	setx DB_DSN "usuario:miclave123@tcp(127.0.0.1:3306)/basedatosnombre?charset=utf8mb4&parseTime=True&loc=Local"
+	*/
+	dsn := os.Getenv("DB_DSN")
+	if dsn == "" {
+		panic("❌ La variable de entorno DB_DSN no está definida")
+	}
 
 	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -18,10 +31,6 @@ func ConectarBD() {
 		panic("❌ Error al conectar con la base de datos: " + err.Error())
 	}
 
+	DB.AutoMigrate(&domain.Usuario{}, &domain.Actividad{}, &domain.Inscripcion{})
 	fmt.Println("✅ Conexión exitosa con GORM")
-}
-
-// GetDB devuelve la instancia de la conexión a la base de datos
-func GetDB() *gorm.DB {
-	return DB
 }
